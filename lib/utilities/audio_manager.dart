eff73bd6199743cf9cd66f0d21d09f723777369f
W1nter-IsHere <w1nter.ish3re@gmail.com>
2023-03-23 22:14:45 -0400

    Fixed cues with ease out not playing properly

--- lib/utilities/audio_manager.dart
+++ lib/utilities/audio_manager.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:echocues/api/models/soundcue.dart';
@@ -11,7 +12,7 @@ class AudioManager {
   AudioManager({
     required this.audioPlayer,
   });
-  
+
   double? totalDuration;
   bool endPremature = false;
   double? prematureStart;
@@ -21,19 +22,19 @@ class AudioManager {
 
   Future<void> dispose() async {
     await audioPlayer.dispose();
-    
+
     if (fadeIn != null) {
       fadeIn!.cancel();
       fadeIn = null;
     }
-    
+
     if (fadeOut != null) {
       fadeOut!.cancel();
       fadeOut = null;
     }
   }
-  
-  Future<void> start(Source source, SoundCue soundCue, {Duration? position, Function()? onComplete}) async {
+
+  Future<void> start(Source source, SoundCue soundCue, {Duration? position, VoidCallback? onComplete}) async {
     await audioPlayer.setPlaybackRate(soundCue.speed);
     await audioPlayer.play(
       source,
@@ -44,34 +45,29 @@ class AudioManager {
     if (!soundCue.easeIn.enabled) {
       await audioPlayer.setVolume(soundCue.volume);
     } else {
-      var fadeInIntervalInMilis = (soundCue.easeIn.duration * 100).round();
+      var fadeInIntervalInMilis = (soundCue.easeIn.duration * 50).round();
       fadeIn = Timer.periodic(Duration(milliseconds: fadeInIntervalInMilis), (timer) async {
-          var currentSecond = timer.tick * fadeInIntervalInMilis / 1000;
-          var mappedValue = Utils.mapRange(currentSecond, 0, soundCue.easeIn.duration, 0, 1);
+        var currentSecond = timer.tick * fadeInIntervalInMilis / 1000;
+        var mappedValue = Utils.mapRange(currentSecond, 0, soundCue.easeIn.duration, 0, 1);
 
-          await audioPlayer.setVolume(eval(mappedValue, soundCue.volume));
-          
-          if (mappedValue == 1) {
-            if (fadeIn == null) return;
-            fadeIn!.cancel();
-            fadeIn = null;
-          }
-        });
+        await audioPlayer.setVolume(eval(mappedValue, soundCue.volume));
+
+        if (mappedValue == 1) {
+          if (fadeIn == null) return;
+          fadeIn!.cancel();
+          fadeIn = null;
+        }
+      });
     }
-    
+
     if (!soundCue.easeOut.enabled) {
       if (onComplete != null) {
-        onCompleteCallback = audioPlayer.onPlayerComplete.listen((_)
-        {
-          onComplete!();
-          onCompleteCallback!.cancel();
-          onCompleteCallback = null;
-        });
+        onCompleteCallback = audioPlayer.onPlayerComplete.listen((_) => cleanup(onComplete));
       }
       return;
     }
 
-    var fadeOutIntervalInMilis = (soundCue.easeOut.duration * 100).round();
+    var fadeOutIntervalInMilis = (soundCue.easeOut.duration * 50).round();
     fadeOut = Timer.periodic(Duration(milliseconds: fadeOutIntervalInMilis), (timer) async {
       var currentSecond = timer.tick * fadeOutIntervalInMilis / 1000;
       double mappedValue;
@@ -82,24 +78,24 @@ class AudioManager {
       } else {
         if (totalDuration == null) {
           var duration = await audioPlayer.getDuration();
-          if (duration != null) {
+          if (duration != null && duration.inMilliseconds != 0) {
             totalDuration = duration.inMilliseconds / 1000.0;
           }
         }
-        
+
         if (totalDuration == null) {
           return;
         }
 
         var fadeStart = totalDuration! - soundCue.easeOut.duration;
-        
+
         if (currentSecond < fadeStart) {
           return;
         }
 
         mappedValue = 1 - Utils.mapRange(currentSecond, fadeStart, totalDuration!, 0, 1);
       }
-      
+
       await audioPlayer.setVolume(eval(mappedValue, soundCue.volume));
 
       if (mappedValue <= 0) {
@@ -109,28 +105,31 @@ class AudioManager {
       }
     });
   }
-  
-  Future<void> stop({Function()? onComplete}) async {
+
+  Future<void> stop({VoidCallback? onComplete}) async {
     if (fadeOut == null) {
       audioPlayer.stop();
       cleanup(onComplete);
       return;
     }
-    
+
     endPremature = true;
   }
-  
-  void cleanup(Function()? onComplete) {
+
+  void cleanup(VoidCallback? onComplete) {
     endPremature = false;
     prematureStart = null;
     fadeIn = null;
     fadeOut = null;
+    totalDuration = null;
+    onCompleteCallback?.cancel();
+    onCompleteCallback = null;
 
     if (onComplete != null) {
-      onComplete!();
+      onComplete();
     }
   }
-  
+
   double eval(double t, double volume) {
     return volume * pow(t , 2);
   }
