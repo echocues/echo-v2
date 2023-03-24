0c21c9cba35842e9b6fa68e16044dc7ab86e09ae
W1nter-IsHere <w1nter.ish3re@gmail.com>
2023-03-23 22:23:08 -0400

    Fixed title

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Echo',
       theme: Utils.defaultDarkTheme(),
       initialRoute: ProjectPageWidget.route,
       routes: {
